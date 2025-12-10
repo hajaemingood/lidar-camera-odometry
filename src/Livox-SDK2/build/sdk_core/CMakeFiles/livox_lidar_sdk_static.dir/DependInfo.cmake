5257fa7b6a34e5c0d3f2d8fdb9a5c15f68ee69e8
hajaemingood <hjm021123@gmail.com>
2025-12-10 13:17:16 +0000

    Vendor packages: ./src/Livox-SDK2/

---
+++ src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/DependInfo.cmake
@@ -0,0 +1,57 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/catkin_ws/src/Livox-SDK2/3rdparty/FastCRC/FastCRCsw.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/__/3rdparty/FastCRC/FastCRCsw.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/base/io_loop.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/base/io_loop.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/base/io_thread.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/base/io_thread.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/base/logging.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/base/logging.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/base/multiple_io/multiple_io_base.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/base/multiple_io/multiple_io_base.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/base/multiple_io/multiple_io_epoll.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/base/multiple_io/multiple_io_epoll.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/base/multiple_io/multiple_io_kqueue.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/base/multiple_io/multiple_io_kqueue.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/base/multiple_io/multiple_io_poll.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/base/multiple_io/multiple_io_poll.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/base/multiple_io/multiple_io_select.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/base/multiple_io/multiple_io_select.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/base/network/unix/network_util.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/base/network/unix/network_util.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/base/thread_base.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/base/thread_base.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/base/wake_up/unix/wake_up_pipe.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/base/wake_up/unix/wake_up_pipe.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/comm/comm_port.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/comm/comm_port.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/comm/generate_seq.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/comm/generate_seq.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/comm/sdk_protocol.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/comm/sdk_protocol.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/command_handler/build_request.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/command_handler/build_request.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/command_handler/command_impl.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/command_handler/command_impl.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/command_handler/general_command_handler.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/command_handler/general_command_handler.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/command_handler/hap_command_handler.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/command_handler/hap_command_handler.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/command_handler/mid360_command_handler.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/command_handler/mid360_command_handler.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/command_handler/parse_lidar_state_info.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/command_handler/parse_lidar_state_info.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/data_handler/data_handler.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/data_handler/data_handler.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/debug_point_cloud_handler/debug_point_cloud_handler.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/debug_point_cloud_handler/debug_point_cloud_handler.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/debug_point_cloud_handler/debug_point_cloud_manager.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/debug_point_cloud_handler/debug_point_cloud_manager.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/device_manager.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/device_manager.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/livox_lidar_sdk.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/livox_lidar_sdk.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/logger_handler/file_manager.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/logger_handler/file_manager.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/logger_handler/logger_handler.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/logger_handler/logger_handler.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/logger_handler/logger_manager.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/logger_handler/logger_manager.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/params_check.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/params_check.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/parse_cfg_file.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/parse_cfg_file.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/upgrade/firmware.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/upgrade/firmware.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/upgrade/livox_lidar_upgrader.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/upgrade/livox_lidar_upgrader.cpp.o"
+  "/root/catkin_ws/src/Livox-SDK2/sdk_core/upgrade_manager.cpp" "/root/catkin_ws/src/Livox-SDK2/build/sdk_core/CMakeFiles/livox_lidar_sdk_static.dir/upgrade_manager.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../sdk_core/../include"
+  "../sdk_core/../3rdparty"
+  "../sdk_core/../3rdparty/spdlog"
+  "../sdk_core/."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
